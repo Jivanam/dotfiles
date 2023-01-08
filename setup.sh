fda36e5d2c780747592d84f6f082fb514eab4806
jivanam <shasondev@gmail.com>
2023-01-08 15:24:53 +0000

    Symlink setup script added

---
+++ setup.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+sgdotfiles="$HOME/.sgdotfiles" #dot files location
+# gitthings="$HOME/Downloads/gitthings"
+# scripts="$HOME/Downloads/gitthings/scripts"
+
+# #Installing Dependencies (arch only)
+# echo "Installing Dependencies"
+# sudo pacman -S i3-gaps rofi kdenlive alacritty ranger dunst neofetch zsh git polybar picom nitrogen arandr udisk2 thunar blueman
+
+#Download Dots
+# echo "Downloading Dotfiles"
+# cd && git clone https://gitlab.com/shason/dotfiles.git
+
+cd $sgdotfiles
+ln -sfn $HOME/.sgdotfiles/.zshenv ~/
+ln -sfn $HOME/.sgdotfiles/.config/alacritty ~/.config
+ln -sfn $HOME/.sgdotfiles/.config/dunst ~/.config
+ln -sfn $HOME/.sgdotfiles/.config/i3 ~/.config
+ln -sfn $HOME/.sgdotfiles/.config/neofetch ~/.config
+ln -sfn $HOME/.sgdotfiles/.config/nitrogen ~/.config
+ln -sfn $HOME/.sgdotfiles/.config/picom ~/.config
+ln -sfn $HOME/.sgdotfiles/.config/polybar ~/.config
+ln -sfn $HOME/.sgdotfiles/.config/ranger ~/.config
+ln -sfn $HOME/.sgdotfiles/.config/rofi ~/.config
+ln -sfn $HOME/.sgdotfiles/.config/sddm ~/.config
+ln -sfn $HOME/.sgdotfiles/.config/zsh ~/.config
+
